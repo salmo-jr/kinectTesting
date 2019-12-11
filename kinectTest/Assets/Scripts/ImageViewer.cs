ea0460d248224dd5deb50c62b320b8cf9479d315
CAR0417140W10-1\Administrador <salmo.sjunior@sp.senac.br>
2019-12-11 21:57:48 -0200

    initial commit

---
+++ kinectTest/Assets/Scripts/ImageViewer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageViewer : MonoBehaviour
+{
+    public MultiSourceManager mMultiSource;
+    public RawImage mRawImage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mRawImage.texture = mMultiSource.GetColorTexture();
+    }
+}
---
+++ kinectTest/Assets/_readme.txt
@@ -0,0 +1,5 @@
+TUTORIAl
+https://www.youtube.com/watch?v=aHGlLxh6a88
+https://www.youtube.com/watch?v=6EkQA3GakFI
+
+https://developer.microsoft.com/en-us/windows/kinect
